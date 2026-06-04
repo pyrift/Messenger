f6620c92635c2a3d6e6b1fb211d9c0e787959cd4
pyrift <sabdumajid91@gmail.com>
2026-06-04 12:40:04 +0500

    qoshildi

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from .views import singe_up_view, login_view
 urlpatterns = [
-    path('', singe_up_view, name='singe_up_view'),
-    path('signup/', login_view, name='signup'),
+    path('signup/', singe_up_view, name='signup'),
     path('login/', login_view, name='login'),
 
 ]
